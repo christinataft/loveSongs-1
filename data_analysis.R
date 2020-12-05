4a78bbdbbc1ef249ec0f904cf267a1d1d0b2505e
tiredmagician <ethancheong@hotmail.sg>
2020-12-06 00:54:41 +0800

    Update analysis, explore string cleaning methods

--- data_analysis.R
+++ data_analysis.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lexicon)
 
 song.data <- read_csv("./datasets/songs_lyrics_short.csv",
                       col_names = c(
@@ -21,4 +22,44 @@ song.data <- read_csv("./datasets/songs_lyrics_short.csv",
                       ),
                       skip = 1)
 
-song.data
+view(song.data)
+
+# Let's work on cleaning the dataset. First, 
+
+song.data.clean <- song.data %>%
+  mutate(lyrics = str_remove_all(lyrics, "\\[.+?\\]")) %>%
+  mutate(lyrics = str_remove_all(lyrics, "\\(.+?\\)")) %>%
+  mutate(lyrics = str_replace_all(lyrics, "â€™", "'")) %>% 
+  mutate(lyrics = str_to_lower(lyrics)) %>%
+  view()
+
+# grady_augmented is a dataset containing 122,806 english words.
+
+song.data.clean %>%
+  filter(lyrics)
+
+# Function to expand contractions - Look deeper into this
+
+FixContractions <- function(string) {
+  string <- str_replace_all(string, "won't", "will not")
+  string <- str_replace_all(string, "can't", "cannot")
+  string <- str_replace_all(string, "n't", " not")
+  string <- str_replace_all(string, "'ll", " will")
+  string <- str_replace_all(string, "'re", " are")
+  string <- str_replace_all(string, "'ve", " have")
+  string <- str_replace_all(string, "'m", " am")
+  string <- str_replace_all(string, "'d", " would")
+  string <- str_replace_all(string, "n'", "ng")
+  string <- str_replace_all(string, "'cause", "because")
+  string <- str_replace_all(string, "it's", "it is")
+  string <- str_replace_all(string, "'s", "")
+  string <- str_replace_all(string, "'less", "unless")
+  return(string)
+}
+
+FixContractions(song.data.clean[[6,4]])
+
+song.data.clean[[6,4]] %>%
+  FixContractions() %>%
+  str_extract_all("[^ -.,!]+")
+
